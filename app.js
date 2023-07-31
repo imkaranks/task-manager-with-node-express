d8f6354715d4354b600806d82082f95be97d1749
Karan <risingphoenix160@gmail.com>
2023-07-31 19:44:48 +0530

    Connect database to the app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 const app = express();
+require('dotenv').config();
 
 // Middlewares
 app.use(express.json());
@@ -14,4 +16,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000;
 
-app.listen(port, console.log(`Express server live at port ${port}`));
+const startServer = async() => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Express server live at port ${port}`));
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+startServer();
